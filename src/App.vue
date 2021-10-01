49fde6698d3b63333fcb49dc820c1686c824eefa
noguchi <02125011a@gmail.com>
2021-10-01 17:26:41 +0900

    fix: できてなかったので元に戻した

--- src/App.vue
+++ src/App.vue
@@ -28,21 +28,6 @@ export default {
       this.isVisibleTopPage = false;
     },
   },
-  mounted: {
-    function() {
-      // 2.初期化
-      // setHeight();
-      const resize = function () {
-        let vh = window.innerHeight * 0.01;
-        document.documentElement.style.setProperty("--vh", `${vh}px`);
-      };
-      // 3.ブラウザのサイズが変更された時・画面の向きを変えた時に再計算する
-      window.addEventListener("resize", resize);
-    },
-    // setHeight() {
-    // 1.関数の定義
-    // },
-  },
 };
 </script>
 
@@ -56,7 +41,5 @@ export default {
 }
 #app {
   background-color: palegreen;
-  min-height: 100vh; /* カスタムプロパティ未対応ブラウザ用のフォールバック */
-  min-height: calc(var(--vh, 1vh) * 100);
 }
 </style>
